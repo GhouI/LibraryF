38f4bba7eeb82c8a151cef57ea87ae265fecae24
Kyushu <flareonverm@outlook.com>
2023-05-19 14:47:00 +0100

    added database

--- src/containers/search/Search.jsx
+++ src/containers/search/Search.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import './search.css'
-const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+import React, { useState } from 'react';
+import './search.css';
 
-export default Search
+const Search = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const getSearchBoxWidth = () => {
+    if (inputValue.length >= 8) {
+      return { width: `${inputValue.length * 10}px`};
+    }
+    return {};
+  };
+
+  return (
+    <div className="verseify__search">
+      <input
+        type="text"
+        placeholder="Search"
+        value={inputValue}
+        onChange={handeInputChange}
+        style={getSearchBoxWidth()}
+        className="search-input"
+      />
+    </div>
+  );
+};
+
+export default Search;
